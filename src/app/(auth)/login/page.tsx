8494e34d06f0b666b9144f6737ee4dff4ab5e2ff
Jean Yves Sadio <jeanyvessadio7@gmail.com>
2026-01-28 07:47:05 +0000

    Ajout des titres

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import LoginPage from "@/components/pages/auth/login/page";
+
+export const metadata: Metadata = {
+  title: "Connexion - Campus Master",
+  description:
+    "Connectez-vous à votre compte Campus Master pour accéder à nos services et ressources exclusives.",
+};
 
 export default function Login() {
   return (
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import SignupPage from "@/components/pages/auth/signup/page";
+
+export const metadata: Metadata = {
+  title: "Inscription - Campus Master",
+  description:
+    "Créez un compte sur Campus Master pour accéder à nos services et ressources exclusives.",
+};
 
 export default function Signup() {
   return (
